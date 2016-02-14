48dad1aaddaf5b53c13fcf82d0cad1bd6ce6e1a1
Erwin De Gier <erwin@edegier.nl>
2016-02-14 15:15:11 +0100

    added consumer

--- mqttclient/src/main/java/mqttclient/Consumer.java
+++ mqttclient/src/main/java/mqttclient/Consumer.java
@@ -1,46 +1,50 @@
 package mqttclient;
 
-import java.io.UnsupportedEncodingException;
 import java.util.UUID;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
-public class Publisher {
+public class Consumer implements MqttCallback {
 
 	private static final String LAST_WILL_TOPIC = "erwindeg/last_will_topic";
 	private static final String CLIENT_ID = UUID.randomUUID().toString();
 	private final String TOPIC = "erwindeg/location";
 	private final MqttClient client;
 
-	public Publisher() throws MqttException {
+	public Consumer() throws MqttException {
 		this.client = new MqttClient("tcp://broker.mqttdashboard.com", CLIENT_ID, new MemoryPersistence());
 
 		final MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
 		mqttConnectOptions.setWill(LAST_WILL_TOPIC, "client offline".getBytes(), 2, true);
 
-		this.client.connect(mqttConnectOptions);
-	}
-
-	public void publishData() throws MqttException, UnsupportedEncodingException {
-		String payload = "{\"clientId\": \"erwin\", \"lat\": \"52.3667\", \"lon\": \"4.9000\"}";
-		MqttMessage message = new MqttMessage(payload.getBytes("UTF-8"));
-		message.setQos(2);
-		message.setRetained(false);
-		client.publish(TOPIC, message);
+		client.connect(mqttConnectOptions);
+		client.setCallback(this);
+		client.subscribe(TOPIC);
 	}
 
 	public static void main(String[] args) throws MqttException {
-		Publisher pub = new Publisher();
-		while (true) {
-			try {
-				pub.publishData();
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
-			}
-		}
+		new Consumer();
 	}
+
+	@Override
+	public void messageArrived(String arg0, MqttMessage message) throws Exception {
+		System.out.println(new String(message.getPayload(), "UTF-8"));
+	}
+	
+	@Override
+	public void connectionLost(Throwable arg0) {
+		//TODO: retry connection
+	}
+
+	@Override
+	public void deliveryComplete(IMqttDeliveryToken token) {
+		//TODO: implement message complete action
+	}
+
 }
